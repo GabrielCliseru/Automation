98de72eec077e6520cc9531de8798cb1edfb93f0
Gabriel Cliseru <gabriel.cliseru@deindeal.ch>
2015-04-22 00:25:41 -0700

    no message

---
+++ src/test/java/cucumber/bdd/Cart.java
@@ -0,0 +1,15 @@
+package cucumber.bdd;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+
+/**
+ * Created by IEUser on 4/22/2015.
+ */
+public class Cart {
+    @And("^Add a product to the cart$")
+    public void Add_a_product_to_the_cart() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+}
