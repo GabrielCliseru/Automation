59e34b6c973a37dd37bea2e5f13eae61765ba348
Gabriel Cliseru <gabriel.cliseru@deindeal.ch>
2015-06-08 10:07:40 +0300

    Added support for properties file for errorMessages

--- src/test/java/cucumber/bdd/Utils.java
+++ src/test/java/cucumber/bdd/Utils.java
@@ -8,23 +8,54 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import sun.text.normalizer.UTF16;
 
 import javax.enterprise.inject.New;
+import java.io.*;
+import java.nio.charset.Charset;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
 import java.util.List;
+import java.io.FileInputStream;
+import java.util.Properties;
+
 
 public class Utils extends SetupDriver {
-//    Login StagingLogin = PageFactory.initElements(driver, Login.class);
-//    Homepage Homepage = PageFactory.initElements(driver, Homepage.class);
-//    MooseheadLogin MooseheadLogin = PageFactory.initElements(driver, MooseheadLogin.class);
-//    MooseheadNewsletter MooseheadNewsletter = PageFactory.initElements(driver, MooseheadNewsletter.class);
-//    Newsletter Newsletter = PageFactory.initElements(driver, Newsletter.class);
 
-    String cityName,cityNameID,newEmailAddress,newPassword;
+    public String cityName,cityNameID;
+    String newEmailAddress = generateEmailAddress();
+    String newPassword = generateString();
+    WebDriverWait wait = new WebDriverWait(driver, 5);
+
+    Properties errorMessages = readErrorMessagesFile("errorMessages");
+
+    public Properties readErrorMessagesFile(String fileName){
+        Properties propFile = new Properties();
+        InputStream input = null;
+        String file = "target/test-classes/"+fileName+".properties";
+        try {
+            input = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // load a properties file
+        try {
+            Reader reader = null;
+            if (input != null) {
+                reader = new InputStreamReader(input, "UTF-8");
+            }
+            propFile.load(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return propFile;
+    }
 
     public void NavigateToAllChannels(){
 
@@ -42,21 +73,26 @@ public class Utils extends SetupDriver {
         }
     }
 
-    public String generateEmailAddress() {
-        String s1, s2, s3;
-        s1 = "";
+    public String generateString(){
+        String randomString = "";
+
         int i;
         Random r = new Random();
         for (i = 0; i < 10; i++) {
             char c = (char) (r.nextInt(26) + 'a');
-            s1 += c;
+            randomString += c;
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        return randomString;
+    }
 
+    public String generateEmailAddress() {
+        String s1, s2, s3;
+        s1 = generateString();
         DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmm");
         Date date = new Date();
         s2 = dateFormat.format(date);
@@ -106,5 +142,22 @@ public class Utils extends SetupDriver {
     public boolean isLanguage(String[] sections, String expectedLanguage){
         //todo: actually debug this crap
         return sections[3].equalsIgnoreCase(expectedLanguage);
+    }
+
+    public void Util_I_should_not_see_the_popup(String pageType){
+        if(pageType.equalsIgnoreCase("login")){
+            while(driver.findElement(By.id("popupPlaceholder")).isDisplayed()) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            org.junit.Assert.assertFalse("newsletter container visible", driver.findElement(By.id("popupPlaceholder")).isDisplayed());
+        }if(pageType.equalsIgnoreCase("new page")){
+            if (driver.findElement(By.cssSelector("#popupPlaceholder_wrapper")).isDisplayed()){
+                driver.findElement(By.cssSelector("#subscriptionLpPopup .skiplink")).click();
+            }
+        }
     }
 }
