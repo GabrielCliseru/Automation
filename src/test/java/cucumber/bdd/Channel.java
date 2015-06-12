3c76ca8d8b67b1a5120cedc6422cb475b2d45686
Gabriel Cliseru <gabriel.cliseru@deindeal.ch>
2015-06-12 09:39:28 +0300

    Changed the static call for the homepage with the step implemented in the homepage Page

--- src/test/java/cucumber/bdd/Channel.java
+++ src/test/java/cucumber/bdd/Channel.java
@@ -1,38 +1,49 @@
 package cucumber.bdd;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 import java.util.Random;
 
 public class Channel extends UtilsDeindeal {
+    Homepage homepage = PageFactory.initElements(driver, Homepage.class);
 
-    @When("^I enter a \"([^\"]*)\" collection$")
-    public int I_enter_a_collection(String arg1)
+    @When("^I enter a random collection$")
+    public int I_enter_a_random_collection()
     {
-        if (arg1.equalsIgnoreCase("random")){
-            List<WebElement> collectionNames = driver.findElements(By.cssSelector("data-tracking-type='collection'"));
-            Random rand = new Random();
-            int i = rand.nextInt(collectionNames.size());
-            //pe asta l-am pus aici ca sa nu mai facem mereu operatiunea de cautare in obiectul sau, deja il folosim de 2 ori
-            //mai jos
-            WebElement currentElement = collectionNames.get(i);
-            String collectionID = currentElement.getAttribute("data-tracking-id");
-            currentElement.click();
-            return Integer.parseInt(collectionID);
-        }else{
-            return 0;
+        List<WebElement> collectionNames;
+        Random rand = new Random();
+
+        if (driver.findElements(By.cssSelector("[data-tracking-type='collection']")).size() != 0){
+            collectionNames = driver.findElements(By.cssSelector("[data-tracking-type='collection']"));
         }
+        else {
+            collectionNames = driver.findElements(By.cssSelector("#collectionsWrapper li"));
+        }
+
+        int i = rand.nextInt(collectionNames.size());
+        //pe asta l-am pus aici ca sa nu mai facem mereu operatiunea de cautare in obiectul sau, deja il folosim de 2 ori
+        //mai jos
+        WebElement currentElement = collectionNames.get(i);
+        String collectionID = "";
+        if(currentElement.isDisplayed()){
+            collectionID = currentElement.getAttribute("data-tracking-id");
+        }else{
+//            currentElement.
+        }
+        currentElement.click();
+
+        return Integer.parseInt(collectionID);
     }
 
     @Given("^I am on \"([^\"]*)\" channel as an existing visitor$")
     public void I_am_on_channel_as_an_existing_visitor(String channelName){
         if(channelName.equalsIgnoreCase("any")){
-            driver.get("http://www.deindeal.ch/de/?src=newsletter");
+            homepage.I_am_on_the_homepage_as_an_existing_visitor();
             List<WebElement> channels = driver.findElements(By.cssSelector(".channel-item"));
             Random rand = new Random();
             int i = rand.nextInt(channels.size());
@@ -54,7 +65,7 @@ public class Channel extends UtilsDeindeal {
     @Given("^I am on \"([^\"]*)\" channel as an existing visitor on \"([^\"]*)\"$")
     public void I_am_on_channel_as_an_existing_visitor_on(String channelName, String language){
         if(channelName.equalsIgnoreCase("any")){
-            driver.get("http://www.deindeal.ch/"+language+"/?src=newsletter");
+            homepage.I_am_on_the_homepage_as_an_existing_visitor_on(language);
             List<WebElement> channels = driver.findElements(By.cssSelector(".channel-item"));
             Random rand = new Random();
             int i = rand.nextInt(channels.size());
