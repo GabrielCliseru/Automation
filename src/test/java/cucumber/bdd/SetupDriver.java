5eb2946b7fdc9b88cbbf0a25c1e9f4ccc4901261
Gabriel Cliseru <gabriel.cliseru@deindeal.ch>
2015-06-16 16:02:52 +0300

    Tested and validated the implementation for MacOS

--- src/test/java/cucumber/bdd/SetupDriver.java
+++ src/test/java/cucumber/bdd/SetupDriver.java
@@ -15,9 +15,15 @@ public class SetupDriver{
             return new FirefoxDriver();
         else if (driver.equalsIgnoreCase("chrome")) {
             String OS = System.getProperty("os.name").toLowerCase();
-            System.setProperty("webdriver.chrome.driver", "target/test-classes/chromedriver.exe");
+
+            System.out.printf(OS);
+
             if(OS.contains("nix") || OS.contains("nux") || OS.indexOf("aix") > 0 ){
                 System.setProperty("webdriver.chrome.driver", "target/test-classes/chromedriver");
+            }else if(OS.contains("mac") ){
+                System.setProperty("webdriver.chrome.driver", "target/test-classes/chromedriver-mac");
+            }else{
+                System.setProperty("webdriver.chrome.driver", "target/test-classes/chromedriver.exe");
             }
             WebDriver webdriverOut = new ChromeDriver();
             webdriverOut.manage().window().maximize();
