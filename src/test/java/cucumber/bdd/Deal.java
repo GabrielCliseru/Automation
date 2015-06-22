84850cf521f635c4f5ed048b66470e32ba06e018
Gabriel Cliseru <gabriel.cliseru@deindeal.ch>
2015-06-22 11:16:16 +0300

    Refactored " I_add_the_deal_to_the_cart" Implemented scrollToElement(WebElement) into UtilsDeindeal. Now we can use it to scroll to elements. Implemented isCollectionPage and enterRandomCollectionFromCollectionPage . With them we can determine if the page is a collection and enter a random collection.

--- src/test/java/cucumber/bdd/Deal.java
+++ src/test/java/cucumber/bdd/Deal.java
@@ -3,10 +3,14 @@ package cucumber.bdd;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -50,46 +54,47 @@ public class Deal extends UtilsDeindeal {
             button = driver.findElement(By.cssSelector(".deal-mod-popup"));
         }
 
-        button.click();
-        //if it is MOD open the modal, make a list of the on-stock variants, pick a random in-stock variant
-        if (type.equals("mod")) {
 
-        }
-        //if it is not an MOD we add to the cart until we finish the list of sizes. Then we change the size. Then we change the product
-        else {
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#popup-cart")));
-            numberOfElementsInTheCart = driver.findElements(By.cssSelector("tr.item.active")).size();
-            int sizesIterator = 0;
-            int numberOfSizes = 0;
-            //the cart is empty
-            if (numberOfElementsInTheCart == 0) {
-                //the product has sizes
-                List<WebElement> sizeDropdownItems = driver.findElements(By.cssSelector("#dropDownArea .productDropDown ul li"));
-                if (sizeDropdownItems.size() != 0) {
-                    numberOfSizes = driver.findElements(By.cssSelector("#dropDownArea .productDropDown ul li")).size();
-                    //go to the next size
-                    do {
-                        sizeDropdownItems.get(sizesIterator).click();
-                        button.click();
-                        numberOfElementsInTheCart = driver.findElements(By.cssSelector("tr.item.active")).size();
-                        sizesIterator++;
-                    } while (numberOfElementsInTheCart > 0 && sizesIterator < sizeDropdownItems.size());
+
+        if (driver.findElements(By.cssSelector(".options.productDropDown")).size() > 0){
+            /*
+                Create a list of all the variants still in stock
+                Shuffle the variant IDs
+                Get the IDs 1 by 1 and try to add them to the cart until 1 has been added.
+                    Alert the system if we couldn't add it but we should and move forward
+            */
+            String selectVariantSelector;
+            if (type.equals("mod")) {
+                selectVariantSelector = ".buy-now-btn";
+            }else{
+                selectVariantSelector = ".options .dropdownList li";
+                //We need to open the drop down selector first, for MOD it should be opened since the previous click
+                driver.findElement(By.cssSelector(".productDropDown .press")).click();
+            }
+            ArrayList<WebElement> allTheVariants = ((ArrayList<WebElement>) driver.findElements(By.cssSelector(selectVariantSelector)));
+            Collections.shuffle(allTheVariants);
+
+            WebElement currentVariant;
+            //note, some of the products might have sizes that are hidden, we should scroll to them before clicking on them
+            for (int i = 0; i < allTheVariants.size(); i++) {
+                currentVariant = allTheVariants.get(i);
+                if(currentVariant.isDisplayed()){
+                    currentVariant.click();
+                }else{
+                    //scroll back and forth trick
+                    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true)", currentVariant);
+                    WebElement detailsWrapper = driver.findElement(By.cssSelector("#detailsWrapper"));
+                    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true)", detailsWrapper);
+                    wait.until(ExpectedConditions.visibilityOf(detailsWrapper));
+                    currentVariant.click();
                 }
             }
-            //the product does not have sizes or all the sizes have been used
-            else if (sizesIterator == numberOfSizes) {
-                //navigate to another channel and collection
-                channel.I_am_on_channel_as_an_existing_visitor("any");
-                channel.I_enter_a_random_collection();
-            }
+            /*
+            click the add to cart button
+            */
+            button.click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#popup-cart")));
+            numberOfElementsInTheCart = driver.findElements(By.cssSelector("tr.item.active")).size();
         }
-
-
-        //coupon
-        //product
-        //mod: add to cart not out of stock mod
-        //wine and gourmet
-        //change product until not out of stock
-        //change product size until not out of stock
     }
 }
